d6990abbbe1a3288992d5cf7411afce6ce746573
SaiKrishnaArige <krishnaarige@gmail.com>
2021-07-27 12:17:03 +0530

    Updates

--- src/test.js
+++
@@ -1,7 +0,0 @@
-{items.map(item=>(
-    <div style={{marginBottom:'20px'}}>
-        <div>Center Name: {item.name}</div>
-        <div>Address: {item.address}</div>
-        <div>{item.vaccine}: {item.fee}</div>
-    </div>
-))} 
